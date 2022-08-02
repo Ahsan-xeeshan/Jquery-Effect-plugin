9351cdaadf43ea0f03fa4c5a64065ed60a49e5bc
Nazmul Ahsan <104188506+Ahsan-xeeshan@users.noreply.github.com>
2022-08-02 23:14:31 +0600

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
